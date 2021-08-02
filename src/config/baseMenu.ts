6fab5cb1323cba87cb9716abb07e9fd74aad72d0
潘少军 <1760406005@qq.com>
2021-08-02 14:43:49 +0800

    cofig 菜单配置

--- src/config/baseMenu.ts
+++ src/config/baseMenu.ts
@@ -1,23 +1,27 @@
 import Config from '../type/Config';
 const baseMenu: Config[] = [
     {
-        id: 4,
+        id: 1,
         name: "web开发",
         chirld: [
             {
-                id: 1,
-                name: "前端基础"
-            }
+                id: 11,
+                name: "基础技术"
+            },
+            {
+                id: 12,
+                name: "框架"
+            },
+            {
+                id: 13,
+                name: "工程"
+            },
         ]
     },
     {
         id: 2,
         name: "算法"
-    },
-    {
-        id: 3,
-        name: "工程"
-    },
+    }
 ]
 
 export default baseMenu;
